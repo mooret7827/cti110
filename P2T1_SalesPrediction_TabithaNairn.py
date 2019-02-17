e3085ac732e4997652ad2a2a70f35f951e8999de
mooret7827 <47708960+mooret7827@users.noreply.github.com>
2019-02-17 02:19:54 -0500

    First Submission/P2T1

---
+++ P2T1_SalesPrediction_TabithaNairn.py
@@ -0,0 +1,16 @@
+# Profit calculator exercise
+# February 9, 2019
+# CTI - 110 P2T1 - Sales Prediction
+# Tabitha Nairn
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+                                
+                                
+
